3856e9fe0a26df96d87009e1ab2b9792288dc741
mhz <mm988977@gmail.com>
2024-01-10 20:39:57 +0200

    add packages and ready to start

---
+++ flutter_application_1/lib/doc_app.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppDoc extends StatelessWidget {
+  const AppDoc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
